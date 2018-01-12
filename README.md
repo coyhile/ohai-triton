20446c43ebc85048d84070fd76321a9e3917ecdb
Coy Hile <coy.hile@coyhile.com>
2018-01-12 17:27:54 -0500

    parent 744ab407e5257bd227c6e7b2eefe5ba62d954c91 author Coy Hile <coy.hile@coyhile.com> 1515796074 -0500 committer Coy Hile <coy.hile@coyhile.com> 1588364537 -0400
    First shot at writing an ohai plugin to get some rudimentary metadata
    from triton.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ohai-triton
 ohai plugin to provide Triton Metadata
+
+This plugin populates Joyent-sepcific metadata considered to be of use to its author.
+
+# Unsupported functionality
+## Triton CNS
+This plugin has no way to populate CNS data into the ohai data as there is so far
+as the author can tell no way to query the value of the metadata tag 
+`triton.cns.services` from inside the instance.
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'ohai-triton'
+maintainer 'Coy Hile'
+maintainer_email 'coy.hile@coyhile.com'
+source_url 'https://github.com/coyhile/ohai-triton'
+issues_url 'https://github.com/coyhile/ohai-triton/issues'
+license 'MPL-2.0'
+description 'ohai plugins for Joyent Triton Datacenter'
+long_description 'ohail plugins for Joyent Triton Datacenter'
+version '1.0.0'
+
+chef_version '>= 12.0'
+
+supports 'redhat'
+supports 'centos'
+supports 'ubuntu'
+supports 'smartos'
+supports 'windows'
+
+depends 'ohai'
+depends 'chef-sugar'
